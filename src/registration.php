60c0bdc16f29c604663a7c13727115a2d77b8a3f
PH3N1X <mondalsayan556@gmail.com>
2019-02-11 00:21:11 +0530

    Refresh the user registration

--- src/registration.php
+++ src/registration.php
@@ -16,18 +16,8 @@
 <body>
 
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "ph3n1x";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-    echo "Connection error";
-} 
+    
+ 
 // // echo "Connected";
 // $conn->query("select database testdb;");
 // $sql = "INSERT INTO testdb.userinfo (name, email, password)
